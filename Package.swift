d43e3a4aab3198a3d8d14170a1ad62705b471930
Roberto Casula <rcasula3@gmail.com>
2019-06-16 17:52:38 +0200

    Added support for Swift Package Manager

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Carbon",
+    platforms: [
+        .iOS(.v9)
+    ],
+    products: [
+        .library(name: "Carbon", targets: ["Carbon"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ra1028/DifferenceKit.git", from: "1.1.2")
+    ],
+    targets: [
+        .target(
+            name: "Carbon",
+            dependencies: ["DifferenceKit"],
+            path: "Sources"),
+        .testTarget(
+            name: "Tests",
+            dependencies: ["Carbon"],
+            path: "Tests"),
+    ]
+)
