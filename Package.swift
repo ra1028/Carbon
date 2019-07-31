79b54a534baaea2ee78f2e736f2c1fb8a2236327
robcas3 <robcas3@users.noreply.github.com>
2019-07-31 07:45:41 +0200

    Adds support for Swift Package Manager (#25)
    * Added support for Swift Package Manager
    
    * fixes the Tests target name
    
    * Fixes version number
    
    * Fixes
    
    * removes useless strings
    
    * removes trailing comma
    
    * removes trailing comma
    
    * Removed .swiftpm and added to .gitignore
    
    * Resolve conflicts

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Carbon",
+    platforms: [
+        .iOS(.v10)
+    ],
+    products: [
+        .library(name: "Carbon", targets: ["Carbon"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ra1028/DifferenceKit.git", .upToNextMinor(from: "1.1.3"))
+    ],
+    targets: [
+        .target(
+            name: "Carbon",
+            dependencies: ["DifferenceKit"],
+            path: "Sources"),
+        .testTarget(
+            name: "Tests",
+            dependencies: ["Carbon"],
+            path: "Tests")
+    ],
+    swiftLanguageVersions: [.v4_2, .v5]
+)
