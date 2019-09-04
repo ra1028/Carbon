05cca94e60005f54d4d80de738deab7ed2f07216
Ryo Aoyama <r.fe51028.r@gmail.com>
2019-09-05 04:48:50 +0900

    Update example

--- Examples/Example-iOS/Sources/Common/Extensions.swift
+++ Examples/Example-iOS/Sources/Common/Extensions.swift
@@ -20,6 +20,12 @@ extension UITableView {
     }
 }
 
+extension UICollectionView {
+    open override func touchesShouldCancel(in view: UIView) -> Bool {
+        return true
+    }
+}
+
 extension UINavigationController {
     open override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         return topViewController?.supportedInterfaceOrientations ?? .portrait
