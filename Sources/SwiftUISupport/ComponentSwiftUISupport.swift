64d13deca522fb095a1a93b6f0a351557942f389
Ryo Aoyama <r.fe51028.r@gmail.com>
2019-10-23 06:48:53 +0900

    Add tests

--- Sources/SwiftUISupport/ComponentSwiftUISupport.swift
+++ Sources/SwiftUISupport/ComponentSwiftUISupport.swift
@@ -1,4 +1,4 @@
-#if canImport(SwiftUI)
+#if canImport(SwiftUI) && canImport(Combine)
 
 import SwiftUI
 
---
+++ Tests/SwiftUISupport/ComponentSwiftUISupportTests.swift
@@ -0,0 +1,79 @@
+#if canImport(SwiftUI) && canImport(Combine)
+
+import XCTest
+import SwiftUI
+@testable import Carbon
+
+@available(iOS 13.0, *)
+final class ComponentSwiftUISupportTests: XCTestCase {
+    func testDisplayLifecycle() {
+        struct TestComponent: Component, View {
+            var willDisplay: () -> Void
+            var didEndDisplay: () -> Void
+
+            func renderContent() -> UIView {
+                UIView()
+            }
+
+            func render(in content: UIView) {}
+
+            func contentWillDisplay(_ content: UIView) {
+                willDisplay()
+            }
+
+            func contentDidEndDisplay(_ content: UIView) {
+                didEndDisplay()
+            }
+        }
+
+        var isWillDisplayCalled = false
+        var isDidEndDisplayCalled = false
+        let component = TestComponent(
+            willDisplay: { isWillDisplayCalled = true },
+            didEndDisplay: { isDidEndDisplayCalled = true }
+        )
+        let hostingController = UIHostingController(rootView: AnyView(component.body))
+
+        XCTAssertFalse(isWillDisplayCalled)
+        XCTAssertFalse(isDidEndDisplayCalled)
+
+        let window = UIWindow()
+        window.rootViewController = hostingController
+        window.isHidden = false
+        window.setNeedsLayout()
+        window.layoutIfNeeded()
+
+        XCTAssertTrue(isWillDisplayCalled)
+        XCTAssertFalse(isDidEndDisplayCalled)
+
+        hostingController.rootView = AnyView(EmptyView())
+        window.setNeedsLayout()
+        window.layoutIfNeeded()
+
+        XCTAssertTrue(isWillDisplayCalled)
+        XCTAssertTrue(isDidEndDisplayCalled)
+    }
+
+    func testReferenceSize() {
+        struct TestComponent: Component, View {
+            static let testSize = CGSize(width: 123, height: 456)
+
+            func renderContent() -> UIView {
+                UIView()
+            }
+
+            func render(in content: UIView) {}
+
+            func referenceSize(in bounds: CGRect) -> CGSize? {
+                Self.testSize
+            }
+        }
+
+        let component = TestComponent()
+        let hostingController = UIHostingController(rootView: component.body)
+
+        XCTAssertEqual(hostingController.view.sizeThatFits(.zero), TestComponent.testSize)
+    }
+}
+
+#endif
